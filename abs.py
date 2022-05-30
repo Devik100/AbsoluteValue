19f639a4dc40a9944feabe0230410ae32be69ec2
Devik100 <74122137+Devik100@users.noreply.github.com>
2022-05-30 09:32:40 -0700

    Add files via upload

--- abs.py
+++ abs.py
@@ -1,6 +1,20 @@
 while True:
-    x=int(input("Type a number here: "))
-    if x > 0:
-        print(x)
-    else:
-        print(-x)
+    try:
+        x=input("Type a number here: ")
+        x=int(x)
+        if x > 0:
+            print(x)
+        else:
+            print(-x)
+    except ValueError:
+        if x == "":
+            print("Please enter a number.")
+        else:
+            try:
+                x=float(x)
+                if x > 0:
+                    print(x)
+                else:
+                    print(-x)
+            except ValueError:
+                print("A number like 11350.6, -12, 5.1476, or -241.7. ")
